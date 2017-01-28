b86e5675a1cbc3856fe19555f29ebcc63d02ea0c
Gary Arnold Seevers <garyarnoldseevers@gmail.com>
2017-01-28 10:09:34 -0600

    Archive R3
    Move two screens to archive

---
+++ archive/R3/index.php
@@ -0,0 +1,152 @@
+<link href="css/style.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no"/>
+<!--<form name="user" method="post" action="https://blooom-api-staging.herokuapp.com/users" id="onboarding-form">-->
+<form name="user" method="post" action="https://blooom-api-staging.herokuapp.com/users" id="onboarding-form">
+	<input type="hidden" name="external" value="true" />
+	<a href="https://secure.blooom.com/signin" id="sign-in-link">Sign In</a>
+	<img src="images/blooom-emblem.png" alt="blooom inc" id="blooom-emblem"/>
+	<!--<ul id="progress-bar">
+		<li class="active">About You</li>
+		<li>Retirement Goals</li>
+		<li>Log In</li>
+	</ul>-->
+	<fieldset>
+		<h2 class="headline"><span class="light">We'd Fill This Out For You</span> But We Don't Have E.S.P.</h2>
+		<div class="form-row">
+			<label for="name">My first name is...</label>
+			<input type="text" name="name" placeholder="First Name" value="" required/>
+			<img src="images/validation-checkmark.png" class="validation-icon"></span>
+		</div><!-- .form-row -->
+		<div class="form-row clearfix" id="gender-row">
+			<label for="sex">I am a...</label>
+			<div class="radio-button-container">
+				<input type="radio" name="sex" value="m" id="radio-button-man"/>
+				<label for="radio-button-man" class="radio-button-label" id="radio-button-label-man">Man</label>
+			</div>
+			<div class="radio-button-container">
+				<input type="radio" name="sex" value="f" id="radio-button-woman"/>
+				<label for="radio-button-woman" class="radio-button-label" id="radio-button-label-woman">Woman</label>
+			</div>
+		</div><!-- .form-row -->
+		<div class="form-row" id="birth-date-row">
+			<label for="birth_date">I was born on...</label>
+			<input type="date" name="birth_date" class="mobile" value="1999-12-31" id="birth_date" />
+			<div class="desktop">
+				<div class="select-container" id="birth-month-select-container">
+					<div class="select-arrow"></div>
+					<select name="birth_month" id="birth-month" required>
+						<option value="Month" selected disabled>Month</option>
+						<?php 
+							$months = array(
+								"01" => "January",
+								"02" => "February",
+								"03" => "March",
+								"04" => "April",
+								"05" => "May",
+								"06" => "June",
+								"07" => "July",
+								"08" => "August",
+								"09" => "September",
+								"10" => "October",
+								"11" => "November",
+								"12" => "December"
+							);
+							foreach($months as $month_numeric => $month_text){
+								?>
+								<option value="<?php echo $month_numeric; ?>"><?php echo $month_text; ?></option>
+								<?php
+							}
+						?>
+					</select>
+				</div><!-- .select-container -->
+				<div class="select-container" id="birth-day-select-container">
+					<div class="select-arrow"></div>
+					<select name="birth_day" id="birth-day" required>
+						<option value="Month" selected disabled>Day</option>
+						<?php 
+							for($counter = 1; $counter <= 31; $counter += 1){
+								?>
+								<option value="<?php echo sprintf('%02d',$counter); ?>" ><?php echo $counter; ?></option>
+								<?php
+							}
+						?>
+					</select>
+				</div><!-- .select-container -->
+				<div class="select-container clearfix" id="birth-year-select-container">
+					<div class="select-arrow"></div>
+					<select name="birth_year" id="birth-year" required>
+						<option value="Year" selected disabled>Year</option>
+						<?php 
+							$current_year = date("Y");
+							$starting_year = $current_year - 14;
+							$max_year = $current_year - 78;
+							for($counter = $starting_year; $counter >= $max_year; $counter -= 1){
+								?>
+								<option value="<?php echo $counter; ?>"><?php echo $counter; ?></option>
+								<?php
+							}
+						?>
+					</select>
+				</div><!-- .select-container -->
+				<input type="hidden" name="birth_date" id="birth_date"/>
+			</div>
+			<p class="validation-message" id="birth-date-validation-message">
+				So you're saying you are <span class="validation-callout" id="birth_age">15</span>
+			</p>
+			<img src="images/validation-checkmark.png" class="validation-icon" id="birth-date-validation-icon"></span>
+		</div><!-- .form-row -->
+		<label for="retirement-age">I'd like to retire at age...</label>
+		<div class="form-row" id="retirement-age-row">
+			<div class="select-container">
+				<div class="select-arrow"></div>
+				<select name="retirement_age" id="retirement-age" required>
+					<?php 
+						for($counter = 35; $counter <= 75; $counter += 1){
+							?>
+							<option value="<?php echo $counter; ?>" <?php if( $counter == 65 ){ echo "selected";} ?> ><?php echo $counter; ?></option>
+							<?php
+						}
+					?>
+				</select>
+			</div><!-- .select-container -->
+			<img src="images/validation-checkmark.png" class="validation-icon" id="retirement-age-validation-icon"></span>
+		</div><!-- .form-row -->
+		<p class="validation-message" id="retirement-age-validation-message">
+			You aren't going to retire <span class="validation-callout" id="years_ago">2 years</span> ago, it's just not going to happen!
+		</p>
+		<img src="images/norton-seal.png" alt="Norton Secured powered by VeriSign" id="norton-seal"/>
+		<div class="next button active" id="next-one"/>Next<span class="button-arrow"></span></div>
+	</fieldset>
+	<fieldset>
+		<h2 class="headline"><span class="light">First Things First.</span> Let's Make Sure We Keep You Safe</h2>
+		<p class="subhead">
+			Why do you need to create an account? Easy. Safety first kids. Plus, if you get distracted (i.e. your boss walks past your desk), you can pick up right where you left off.
+		</p>
+		<div class="form-row" id="email-row">
+			<label for="email">Email Address</label>
+			<input type="email" name="email" placeholder="email address" id="email" />
+			<img src="images/validation-checkmark.png" class="validation-icon" id="email-validation-icon"></span>
+		</div><!-- .form-row -->
+		<div class="form-row" id="password-row">
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password"/>
+			<img src="images/validation-checkmark.png" class="validation-icon" id="password-validation-icon"></span>
+			<div id="password-strength-meter">
+				<img src="images/security-icon.png" id="security-icon" alt="padlock" />
+				<span id="password-strength-bar"></span>
+				<span id="password-strength-message"></span>
+			</div>
+			<p class="validation-message" id="password-validation-message">
+				Try adding numbers, symbols, or different case letters
+			</p>
+		</div><!-- .form-row -->
+		<input type="hidden" name="password_verify" value="" id="password_verify"/>
+		<div class="previous button"/>
+			<span class="button-arrow"></span>Previous
+		</div>
+		<input type="submit" name="submit" class="submit button active" value="Submit" id="submit"/>
+	</fieldset>
+</form>
+<script src="http://thecodeplayer.com/uploads/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/zxcvbn.js"></script>
+<script src="js/javascript.js" type="text/javascript"></script>
---
+++ archive/R3/js/javascript.js
@@ -0,0 +1,359 @@
+// initialize variables
+var current_fieldset;
+var next_fieldset;
+var previous_fieldset;
+var left;
+var opacity;
+var scale;
+var animating;
+// initialize validation variables
+$name_valid = false;
+$birth_date_valid = false;
+$sex_valid = false;
+$retirement_age_valid = false;
+$email_valid = false;
+$password_valid = false;
+
+// Create an array of password messages
+var password_message = [];
+// Push objects into password array to store settings
+password_message.push({
+	width: "10px",
+	background: "rgb(254,133,48)",
+	strength_message: "too weak",
+	validation_message: "block",
+	validation_icon: "images/validation-x.png",
+	valid: false
+});
+password_message.push({
+	width: "25px",
+	background: "rgb(255, 167, 114)",
+	strength_message: "could be better",
+	validation_message: "block",
+	validation_icon: "images/validation-x.png",
+	valid: false
+});
+password_message.push({
+	width: "50px",
+	background: "rgb(167, 205, 82)",
+	strength_message: "shows potential",
+	validation_message: "none",
+	validation_icon: "images/validation-checkmark.png",		
+	valid: true
+});
+password_message.push({
+	width: "100px",
+	background: "rgb(136, 186, 39)",
+	strength_message: "well played",
+	validation_message: "none",
+	validation_icon: "images/validation-checkmark.png",	
+	valid: true
+});
+password_message.push({
+	width: "150px",
+	background: "rgb(104, 141, 26)",
+	strength_message: "wowzer",
+	validation_message: "none",
+	validation_icon: "images/validation-checkmark.png",	
+	valid: true
+});
+
+$(document).ready(function(){
+	$('.next, .submit').removeClass("active");
+	$('.next, .submit').addClass("inactive");
+});
+
+$("input[type=text]").keyup(function(){
+	validate(this);
+});
+
+$("input[type=radio]").change(function(){
+	validate(this);
+});
+
+$("input[type=radio]").change(function(){
+	validate(this);
+});
+
+$("input[type=date]").change(function(){
+	validate_birth_date_mobile();
+	if($('.desktop').length){
+		$('.desktop').remove();
+	}
+});
+
+$("#birth-month, #birth-day, #birth-year").change(function(){
+	validate_birth_date();
+	if($('.mobile').length){
+		$('.mobile').remove();
+	}
+});
+
+$("#retirement-age").change(function(){
+	validate_retirement_age();
+});
+
+$("#email").keyup(function(){
+	validate_email();
+});
+
+$("#password").keyup(function(){
+	validate_password(this);
+});
+
+
+function validate($selected_element){
+	$value = $($selected_element).val();
+	if($value != ""){
+		$this_name = $($selected_element).attr("name");
+		window["$" + $this_name + "_valid"] = true;
+		$($selected_element).siblings(".validation-icon").css('display','block');
+	}else{
+		window["$" + $this_name + "_valid"] = false;
+	}
+	if(validate_fieldset_one()){
+		activate_button("#next-one");
+	}
+}
+function validate_birth_date_mobile(){
+	$birth_date_value = $('#birth_date').val();
+	if($birth_date_value != null){
+		$current_date = new Date();
+		$current_year = $current_date.getFullYear();
+		$birth_date = new Date($birth_date_value);
+		$birth_year = $birth_date.getFullYear();
+		$years_old = $current_year - $birth_year;
+		$("#birth_age").html($years_old);
+		$("#birth-date-validation-icon").css('display','block');
+		$("#birth-date-validation-message").css('display','block');
+		$birth_date_valid = true;
+	}else{
+		$("#birth-date-validation-icon").css('display','none');
+		$("#birth-date-validation-message").css('display','none');
+		$birth_date_valid = false;
+	}
+	validate_retirement_age();
+	if(validate_fieldset_one()){
+		activate_button("#next-one");
+	}else{
+		deactivate_button("#next-one");
+	}
+}
+function validate_birth_date(){
+	$birth_month = $('#birth-month').val();
+	$birth_day = $('#birth-day').val();
+	$birth_year = $('#birth-year').val();
+	if($birth_month == 04 || $birth_month == 06 || $birth_month == 09 || $birth_month == 11){
+		$('#birth-day option[value=31]').removeAttr('selected');
+		$('#birth-day option[value=31]').css('display','none');
+	}
+	if($birth_month == 02){
+		$('#birth-day option[value=31]').removeAttr('selected');
+		$('#birth-day option[value=31]').css('display','none');
+		$('#birth-day option[value=30]').removeAttr('selected');
+		$('#birth-day option[value=30]').css('display','none');
+		if(!is_leap_year($birth_year)){
+			$('#birth-day option[value=29]').css('display','none');
+			$('#birth-day option[value=29]').removeAttr('selected');
+		}else{
+			$('#birth-day option[value=29]').css('display','block');
+		}
+	}else{
+		$('#birth-day option[value=30]').css('display','block');
+		$('#birth-day option[value=31]').css('display','block');
+	}
+	if($birth_month != null && $birth_day != null && $birth_year != null){
+		$birth_date = $birth_year + "-" + $birth_month + "-" + $birth_day;
+		$("#birth_date").val($birth_date);
+		$current_date = new Date();
+		$current_year = $current_date.getFullYear();
+		$years_old = $current_year - $birth_year;
+		$("#birth_age").html($years_old);
+		$("#birth-date-validation-icon").css('display','block');
+		$("#birth-date-validation-message").css('display','block');
+		$birth_date_valid = true;
+	}else{
+		$("#birth-date-validation-icon").css('display','none');
+		$("#birth-date-validation-message").css('display','none');
+		$birth_date_valid = false;
+	}
+	validate_retirement_age();
+	if(validate_fieldset_one()){
+		activate_button("#next-one");
+	}else{
+		deactivate_button("#next-one");
+	}
+}
+
+function is_leap_year($year)
+{
+  return (($year % 4 == 0) && ($year % 100 != 0)) || ($year % 400 == 0);
+}
+
+function validate_retirement_age(){
+	$birth_date_value = $('#birth_date').val();
+	$birth_date = new Date($birth_date_value);
+	$birth_year = $birth_date.getFullYear();
+	$current_date = new Date();
+	$current_year = $current_date.getFullYear();
+	$years_old = parseInt($current_year - $birth_year);
+	$retirement_age = parseInt($('#retirement-age').val());
+	if($retirement_age >= $years_old){
+		$retirement_age_valid = true;
+		$('#retirement-age-validation-icon').css('display','block');
+		$('#retirement-age-validation-icon').attr('src','images/validation-checkmark.png');
+		$('#retirement-age-validation-message').css('display','none');
+	}else{
+		$years_till_retirement = $years_old - $retirement_age;
+		$retirement_age_valid = false;
+		$('#retirement-age-validation-message').css('display','block');
+		$('#retirement-age-validation-icon').attr('src','images/validation-x.png');
+		if($years_till_retirement == 1){
+			$('#retirement-age-validation-message .validation-callout').html('1 year');
+		}else{
+			$('#retirement-age-validation-message .validation-callout').html($years_till_retirement + ' years');
+		}
+	}
+	if(validate_fieldset_one()){
+		activate_button("#next-one");
+	}else{
+		deactivate_button("#next-one");
+	}
+}
+
+function validate_email(){
+	$email = $("#email").val();
+	if(regex_email($email)){
+		$email_valid = true;
+		$('#email-validation-icon').css('display','block');
+  		validate_fieldset_two()
+	}else{
+		$email_valid = false;
+		$('#email-validation-icon').css('display','none');
+		validate_fieldset_two()
+	}
+}
+
+
+function validate_password(){
+	var password_field = document.getElementById('password');
+	var password_field_value = password_field.value;
+	var result = zxcvbn(password_field_value);
+	var password_strength = result.score;
+	// Set display of password message by accesing object values with password strength as index
+	$("#password-strength-bar").css('width',password_message[password_strength].width);
+	$("#password-strength-bar").css('background-color',password_message[password_strength].background);
+	$("#password-strength-message").html(password_message[password_strength].strength_message);
+	$("#password-validation-message").css('display',password_message[password_strength].validation_message);
+	$("#password").siblings(".validation-icon").css('display','block');
+	$("#password").siblings(".validation-icon").attr('src',password_message[password_strength].validation_icon);
+	$password_valid = password_message[password_strength].valid;
+	validate_fieldset_two()
+}
+
+
+function regex_email($email_to_test){
+  	var $email_regex_pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  	return $email_regex_pattern.test($email_to_test);
+}
+
+
+function validate_fieldset_one(){
+	if($name_valid == true && $birth_date_valid == true && $sex_valid == true && $retirement_age_valid == true){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function validate_fieldset_two(){
+	if($email_valid == true && $password_valid == true){
+		$("#submit").attr('type','submit');
+		activate_button("#submit");
+		return true;
+	}else{
+		$("#submit").attr('type','button');
+		deactivate_button("#submit");
+		return false;
+	}
+}
+
+function activate_button($selected_button){
+	$($selected_button).addClass("active");
+	$($selected_button).removeClass("inactive");
+}
+
+function deactivate_button($selected_button){
+	$($selected_button).addClass("inactive");
+	$($selected_button).removeClass("active");
+}
+
+$("#next-one").click(function(){
+	if(validate_fieldset_one()){
+		show_next_fieldset(this);
+		$("#submit").attr('type','button');
+	}
+});
+
+function show_next_fieldset($clicked_button){
+	if(animating) return false;
+	animating = true;
+	
+	current_fieldset = $($clicked_button).parent();
+	next_fieldset = $($clicked_button).parent().next();
+	
+	//activate next step on progress-bar using the index of next_fieldset
+	$("#progress-bar li").eq($("fieldset").index(next_fieldset)).addClass("active");
+	
+	//show the next fieldset
+	next_fieldset.show(); 
+	//hide the current fieldset with style
+	current_fieldset.animate({opacity: 0}, {
+		step: function(now, mx) {
+			left = (now * 50)+"%";
+			opacity = 1 - now;
+			current_fieldset.css({'transform': 'scale('+scale+')'});
+			next_fieldset.css({'left': left, 'opacity': opacity});
+		}, 
+		duration: 300, 
+		complete: function(){
+			current_fieldset.hide();
+			animating = false;
+		}, 
+		easing: 'swing'
+	});
+}
+
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fieldset = $(this).parent();
+	previous_fieldset = $(this).parent().prev();
+	
+	//de-activate current step on progress-bar
+	$("#progress-bar li").eq($("fieldset").index(current_fieldset)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fieldset.show(); 
+	//hide the current fieldset with style
+	current_fieldset.animate({opacity: 0}, {
+		step: function(now, mx) {
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fieldset to 1 as it moves in
+			opacity = 1 - now;
+			current_fieldset.css({'left': left});
+			previous_fieldset.css('opacity', opacity);
+		}, 
+		duration: 300, 
+		complete: function(){
+			current_fieldset.hide();
+			animating = false;
+		}, 
+		easing: 'swing'
+	});
+});
+
+$(".submit").click(function(){
+})
+
