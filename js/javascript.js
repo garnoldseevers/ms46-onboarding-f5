baf62f47e0ab85d0553f792a80534122a9334d96
Gary Arnold Seevers <garyarnoldseevers@gmail.com>
2017-01-31 13:18:00 -0600

    Move DOM assignments to head
    increase speed of functions by moving dom requests to head and
    assigning to variables

--- js/javascript.js
+++ js/javascript.js
@@ -20,6 +20,18 @@ var password_valid = false;
 // initialize time
 var current_date = new Date();
 var current_year = current_date.getFullYear();
+// initialize DOM elements
+var birth_month_dom = document.getElementById("birth-month");
+var birth_day_dom = document.getElementById("birth-day");
+var birth_date_dom = document.getElementById("birth-date");
+var birth_date_mobile_dom = document.getElementById("birth-date-mobile");
+var birth_age_dom = document.getElementById("birth_age");
+var birth_year_dom = document.getElementById("birth-year");
+var retirement_age_dom = document.getElementById("retirement-age");
+var years_ago_dom = document.getElementById("years-ago");
+var email_dom = document.getElementById("email");
+var password_dom = document.getElementById('password');
+var password_strength_message_dom = document.getElementById("password-strength-message");
 
 // initialize javascript enabled state of form
 $(document).ready(function(){
@@ -44,7 +56,8 @@ function get_dcode(){
 function get_variable_from_query(variable){
 	var query = window.location.search.substring(1);
 	var parameters = query.split("&");
-	for (var i = 0; i < parameters.length; i++) {
+	var parameters_length = parameters.length;
+	for (var i = 0; i < parameters_length; i++) {
 		var parameter = parameters[i].split("=");
 		if(parameter[0] == variable){
 			return parameter[1];
@@ -161,13 +174,13 @@ function adjust_gender_validation_messages(){
 */
 
 function validate_birth_date_mobile(){
-	var birth_date_value = sanitize_input(document.getElementById("birth-date-mobile").value);
-	document.getElementById("birth-date").value = birth_date_value;
+	var birth_date_value = sanitize_input(birth_date_mobile_dom.value);
+	birth_date_dom.value = birth_date_value;
 	if(birth_date_value != null){
 		birth_date = new Date(birth_date_value);
 		birth_year = birth_date.getFullYear();
 		years_old = current_year - birth_year;
-		document.getElementById("birth_age").innerHTML = years_old;
+		birth_age_dom.innerHTML = years_old;
 		$('#birth-date-validation-icon').attr('src','images/validation-checkmark.png');
 		$("#birth-date-validation-icon").css('display','block');
 		$("#birth-date-age-display").css('display','block');
@@ -181,9 +194,9 @@ function validate_birth_date_mobile(){
 }
 
 function validate_birth_date(){
-	var birth_month = sanitize_input(document.getElementById("birth-month").value);
-	var birth_day = sanitize_input(document.getElementById("birth-day").value);
-	var birth_year = sanitize_input(document.getElementById("birth-year").value);
+	var birth_month = sanitize_input(birth_month_dom.value);
+	var birth_day = sanitize_input(birth_day_dom.value);
+	var birth_year = sanitize_input(birth_year_dom.value);
 	if(birth_month != null && birth_month != "Month" && birth_day != null && birth_day != "Day" && birth_year != null && birth_year != "Year"){
 		var birth_date = birth_year + "-" + birth_month + "-" + birth_day;
 		$("#birth-date").val(birth_date);
@@ -201,8 +214,8 @@ function validate_birth_date(){
 }
 
 function set_days_in_month(){
-	var birth_month = sanitize_input(document.getElementById("birth-month").value);
-	var birth_year = sanitize_input(document.getElementById("birth-year").value);
+	var birth_month = sanitize_input(birth_month_dom.value);
+	var birth_year = sanitize_input(birth_year_dom.value);
 	if(birth_month == 02){
 		$('#birth-day option[value=31]').removeAttr('selected');
 		$('#birth-day option[value=31]').css('display','none');
@@ -233,12 +246,12 @@ function is_leap_year($year){
 */
 
 function validate_retirement_age(){
-	var birth_date_value = sanitize_input(document.getElementById("birth-date").value);
+	var birth_date_value = sanitize_input(birth_date_dom.value);
 	var birth_date = new Date(birth_date_value);
 	var birth_year = birth_date.getFullYear();
 	if(!isNaN(birth_year)){
 		var years_old = parseInt(current_year - birth_year);
-		var retirement_age = parseInt(sanitize_input(document.getElementById("retirement-age").value));
+		var retirement_age = parseInt(sanitize_input(retirement_age_dom.value));
 		if(retirement_age >= years_old){
 			retirement_age_valid = true;
 			display_validation_messages("retirement-age",true);
@@ -251,9 +264,9 @@ function validate_retirement_age(){
 			$('#retirement-age-display').css('display','block');
 			$('#retirement-age-validation-icon').attr('src','images/validation-x.png');
 			if(years_till_retirement == 1){
-				document.getElementById("years-ago").innerHTML = "1 year";
+				years_ago_dom.innerHTML = "1 year";
 			}else{
-				document.getElementById("years-ago").innerHTML = years_till_retirement + " years";
+				years_ago_dom.innerHTML = years_till_retirement + " years";
 			}
 		}
 	}
@@ -265,7 +278,7 @@ function validate_retirement_age(){
 */
 
 function validate_email(){
-	var email_field_value = sanitize_input(document.getElementById("email").value);
+	var email_field_value = sanitize_input(email_dom.value);
 	if(test_email_pattern(email_field_value)){
 		email_valid = true;
 		$("#email").siblings(".validation-icon").attr('src','images/validation-checkmark.png');
@@ -341,7 +354,7 @@ password_message.push({
 
 function validate_password(){
 	// assign value of password field to variable, we do NOT sanitize this. I am told that all characters are accepted
-	var password_field_value = document.getElementById('password').value;
+	var password_field_value = password_dom.value;
 	// pass password field value to zxcvbn function and assign result to variable
 	var result = zxcvbn(password_field_value);
 	// assign the value of the result's score property to a variable
@@ -349,7 +362,7 @@ function validate_password(){
 	// use the password_strength variable as an index to access the appropriate object in the password_message array and apply the properties to the page display
 	$("#password-strength-bar").css('width',password_message[password_strength].width);
 	$("#password-strength-bar").css('background-color',password_message[password_strength].background);
-	document.getElementById("password-strength-message").innerHTML = password_message[password_strength].strength_message;
+	password_strength_message_dom.innerHTML = password_message[password_strength].strength_message;
 	// check if the valid property of the password_message object is true
 	if(password_message[password_strength].valid == true){
 		// if true, hide validation message and show checkmark
